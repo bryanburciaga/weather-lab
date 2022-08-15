b2dcd1db6d592deb88c4d68bb9db0b3fb47f2a27
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-08-15 11:58:01 -0600

    wip: try 1

---
+++ script.js
@@ -0,0 +1,53 @@
+// constants
+
+const URL = 'https://openweathermap.org';
+const API_KEY = '900b111bf759049f45cf0fa4740e0d24';
+
+
+
+const $main = $('main');
+const $form = $('form');
+const $input = $('input[type="text"]');
+// event listeners
+$form.on('submit', handleSubmit);
+
+// functions
+handleSubmit();
+
+function handleSubmit(event) {
+    event && event.preventDefault(''); 
+
+    const city = $input.val('');
+    const state = $input.val('');
+    const country = $input.val('');
+    const limit = $input.val('');
+    // immediately exit the function if no title provided
+    
+    const promise = $.ajax(`${URL}/geo/1.0/direct?q=${city}, ${state}, ${country}&limit=${limit}&appid=${API_KEY}`);
+    
+   
+    
+    promise.then(
+        function(data) {
+            // success callback
+            console.log('Data: ', data);
+            render(data);
+        }, 
+        function(error) {
+            // failure callback
+            console.log('Error: ', error);
+        }
+        );
+    }
+    
+    // test 
+    
+function render(weatherData) { // render means the process of visualizing data
+    $main.html(`
+        <h3>"City:${weatherData.city}" <h3>
+        <p>"Temperature: ${weatherData.temperature}" <p>
+        <p>"Feels like: ${weatherData.feelsLike}" <p>
+        <p>"Weather: ${weatherData.weather}" <p>
+    `)
+}    
+
